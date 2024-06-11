43c02611eb9c2c4c94f88107cf3caed5773aea03
bburwinkel <burwinkelben1@gmail.com>
2024-06-10 21:17:29 -0400

    added a deal and new hand function

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+# Load imports
 from art import logo
 from replit import clear
 import random
 
+# Create the deal function
+def deal():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+# Create the new hand function
+def your_new_hand():
+  your_cards = []
+  your_cards.append(deal())
+  your_cards.append(deal())
+  return your_cards
