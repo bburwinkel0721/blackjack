3e84bb037fad8904da69f1db0b51d20d08b62d78
bburwinkel <burwinkelben1@gmail.com>
2024-06-10 21:22:15 -0400

    more functions for the game

--- main.py
+++ main.py
@@ -15,3 +15,35 @@ def your_new_hand():
   your_cards.append(deal())
   your_cards.append(deal())
   return your_cards
+
+# Create a function for the computers hand
+def computers_new_hand():
+  computers_cards = []
+  computers_cards.append(deal())
+  computers_cards.append(deal())
+  return computers_cards
+
+# Create a function for tracking your current score
+def current_your_score():
+  your_score = 0
+  for card in range(len(your_cards)):
+    your_score += your_cards[card]
+  return your_score
+
+# Create a function for tracking the computer's score
+def current_computers_score():
+  computers_score = 0
+  for card in range(len(computers_cards)):
+    computers_score += computers_cards[card]
+  return computers_score
+
+# Create a function to check if we are still playing
+def still_playing(answer):
+  if answer == "y":
+    play = True
+    clear()
+    print(logo)
+  else:
+    play = False
+    clear()
+  return play
